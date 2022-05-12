127ee2283267bdb76dbdc55ee9a378813ae05a56
Ediberto Ponce <ediberto01@gmail.com>
2022-05-11 21:48:06 -0700

    empty component files intened to be used in the near future

---
+++ components/Dashboard.js
@@ -0,0 +1,26 @@
+import Link from "next/link"
+
+export default function Dashboard () {
+  return (
+    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+
+          <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+            <li>
+              <Link href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                <a>Add a Geocache</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                <a>Find a Geocache</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                <a>Create a Meetup</a>
+              </Link>
+            </li>
+          </ul>
+    </nav>
+  )
+}
