7a483fa2f01c8e50726034c4b814e8a1973f6c0b
Isaiah Burkes <idkburkes@gmail.com>
2022-05-13 09:51:52 -0400

    Added new upload component to CacheForm

--- components/CacheForm.js
+++ components/CacheForm.js
@@ -1,4 +1,13 @@
+import Upload from './Upload.jsx';
+
 export default function CacheForm() {
+
+  // Do stuff here after the image is successfully uploaded to AWS S3
+  const handleUploadImageFinished = () => {
+    console.log('Image successfully uploaded.')
+  }
+  
+
   return (
     <>
       <div className="flex justify-center">
@@ -106,18 +115,7 @@ export default function CacheForm() {
                           />
                         </svg>
                         <div className="flex text-sm text-gray-600">
-                          <label
-                            htmlFor="file-upload"
-                            className="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
-                          >
-                            <span>Upload a file</span>
-                            <input
-                              id="file-upload"
-                              name="file-upload"
-                              type="file"
-                              className="sr-only"
-                            />
-                          </label>
+                            <Upload handleFinish={handleUploadImageFinished} />
                           <p className="pl-1">or drag and drop</p>
                         </div>
                         <p className="text-xs text-gray-500">
