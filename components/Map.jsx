56e645b858a75f356251f555d73506dee0feba93
Fadab2 <ali_berg1@yahoo.com>
2022-05-13 15:43:59 -0700

    added a funtion to add 5 hardcoded locations and display the locations on top

--- components/Map.jsx
+++ components/Map.jsx
@@ -4,25 +4,29 @@ import LocationPin from 'google-map-react'
 
 
 const center = {
-  lat: 47.62548143404639,
-  lng: -122.34996404916232
-}
-const location = {
-  address: '400 Broad St, Seattle, WA 98109',
-  lat: 47.62548143404639,
-  lng: -122.34996404916232
+  lat: 47.6062,
+  lng: -122.3321
 }
 
-const Map = () => (
+const Map = () => {
+  const locations = [
+    { location: 'Seattle, WA', lat: 47.6062, lng: 122.3321},
+    { location: 'Portland, OR', lat: 45.5152, lng: 122.6784 },
+    { location: 'New York City, NY', lat: 40.7128, lng: 74.006 },
+    { location: 'Los Angeles, CA', lat: 34.0522, lng: 118.2437 },
+    { location: 'Las Vegas, NV', lat: 36.1716, lng: 115.1391 }
+  ];
 
+return (
   <div>
-    <h2>dudes of color</h2>
+    {locations.map((loc) => (<p>{loc.location}   {loc.lat} N, { loc.lng} W</p>)
+    )}
 
     <div style={{ height: '100vh', width: '100vh' }}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY}}
         defaultCenter={center}
-        defaultZoom={11}
+        defaultZoom={12}
 
       >
         {/* <LocationPin
@@ -33,7 +37,8 @@ const Map = () => (
       </GoogleMapReact>
     </div>
   </div>
-)
+);
+      }
 export default Map
 
 // const Map = ({ location, zoomLevel }) => (
@@ -55,4 +60,7 @@ export default Map
 //       </GoogleMapReact>
 //     </div>
 //   </div>
+
+
+
 
