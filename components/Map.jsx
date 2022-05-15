83a8eaca5e708f3d8f7a19c1f6b14e6ad45de154
Isaiah Burkes <idkburkes@gmail.com>
2022-05-14 20:37:15 -0400

    Added ability to show map markers on map

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,56 +1,115 @@
+
 import React from 'react'
-import GoogleMapReact from 'google-map-react'
-import LocationPin from 'google-map-react'
+import { GoogleMap, useJsApiLoader, Marker, InfoWindow } from '@react-google-maps/api';
+
+import { useState } from 'react'
+
+ export default function MediumMap() {
+
+    const [ selected, setSelected ] = useState({});
+  
+    const onSelect = item => {
+      setSelected(item);
+    }
+
+    const { isLoaded } = useJsApiLoader({
+      id: 'google-map-script',
+      googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY
+    })
+  
+    const [map, setMap] = React.useState(null)
+
+    const locations = [
+        {
+          name: "Location 1",
+          location: { 
+            lat: 41.3954,
+            lng: 2.162 
+          },
+        },
+        {
+          name: "Location 2",
+          location: { 
+            lat: 41.3917,
+            lng: 2.1649
+          },
+        },
+        {
+          name: "Location 3",
+          location: { 
+            lat: 41.3773,
+            lng: 2.1585
+          },
+        },
+        {
+          name: "Location 4",
+          location: { 
+            lat: -3.9,
+            lng: -39.0
+          },
+        },
+        {
+          name: "This is the secret cache",
+          location: { 
+            lat: -3.745,
+            lng: -38.523
+          },
+        }
+      ];
+
+    const containerStyle = {
+        width: '100vh',
+        height: '100vh'
+      };
+      
+      const center = {
+        lat: -3.745,
+        lng: -38.523
+      };
+  
+    const onLoad = React.useCallback(function callback(map) {
+      const bounds = new window.google.maps.LatLngBounds(center);
+      map.fitBounds(bounds);
+      setMap(map)
+    }, [])
+  
+    const onUnmount = React.useCallback(function callback(map) {
+      setMap(null)
+    }, [])
 
 
-const center = {
-  lat: 47.62548143404639,
-  lng: -122.34996404916232
+    return isLoaded ? (
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+          onLoad={onLoad}
+          onUnmount={onUnmount}
+        >
+          {
+            locations.map(item => {
+              return (
+              <Marker 
+              key={item.name} 
+              position={item.location}
+              onClick={() => onSelect(item)}
+              />
+              )
+            })
+         }
+         {
+            selected.location && 
+            (
+              <InfoWindow
+              position={selected.location}
+              clickable={true}
+              onCloseClick={() => setSelected({})}
+            >
+            <p>{selected.name}</p>
+            </InfoWindow>
+            )
+         }
+          <></>
+        </GoogleMap>
+    ) : <></>
 }
-const location = {
-  address: '400 Broad St, Seattle, WA 98109',
-  lat: 47.62548143404639,
-  lng: -122.34996404916232
-}
-
-const Map = () => (
-
-  <div>
-    <div style={{ height: '100vh', width: '100vh' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY}}
-        defaultCenter={center}
-        defaultZoom={11}
-
-      >
-        {/* <LocationPin
-          lat={location.lat}
-          lng={location.lng}
-          text={location.address}
-        /> */}
-      </GoogleMapReact>
-    </div>
-  </div>
-)
-export default Map
-
-// const Map = ({ location, zoomLevel }) => (
-
-//   <div>
-//     <h2 className="map-h2">dudes of color</h2>
-
-//     <div>
-//       <GoogleMapReact
-//         bootstrapURLKeys={{ key: '' }}
-//         defaultCenter={location}
-//         defaultZoom={zoomLevel}
-//       >
-//         <LocationPin
-//           lat={location.lat}
-//           lng={location.lng}
-//           text={location.address}
-//         />
-//       </GoogleMapReact>
-//     </div>
-//   </div>
-
