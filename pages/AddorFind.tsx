3dc9d325961d767410bcfcc59ebc238cb1ed5ca4
Ediberto Ponce <ediberto01@gmail.com>
2022-05-18 15:02:28 -0700

    Got rid of old import

--- pages/AddorFind.tsx
+++ pages/AddorFind.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import CacheForm from '../components/CacheForm'
-import Dashboard from '../components/Dashboard'
 
 const AddorFind: NextPage = () => {
   return <CacheForm />
