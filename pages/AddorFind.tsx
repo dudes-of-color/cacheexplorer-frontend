f5cfc2b6667d9ba845de9cff7df100ef04352b0b
Ediberto Ponce <ediberto01@gmail.com>
2022-05-18 14:41:02 -0700

    Moved 'Find Cache'. Implements both feature into one page

---
+++ pages/AddorFind.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next'
+import CacheForm from '../components/CacheForm'
+import Dashboard from '../components/Dashboard'
+
+const AddorFind: NextPage = () => {
+  return <CacheForm />
+}
+
+export default AddorFind
--- pages/UserPage.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from 'next'
-import Dashboard from '../components/Dashboard'
-
-const userPage: NextPage = () => {
-  return <Dashboard />
-}
-
-export default userPage
