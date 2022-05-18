3718be9f36cecbe2c8b7af9e0f546ca3e58b1bdc
Isaiah Burkes <idkburkes@gmail.com>
2022-05-18 12:24:26 -0400

    Added requirement for passwords to match on sign up form

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -16,6 +16,7 @@ export default function LoginForm() {
   const [password, setPassword] = useState('')
   const [passwordConfirmation, setPasswordConfirmation] = useState('')
   const [email, setEmail] = useState('')
+  const [passwordsMatch, setPasswordsMatch] = useState(true)
 
   // Capture login request fields and pass to auth context for global login
   const handleSignUpRequest = async (event) => {
@@ -42,13 +43,25 @@ export default function LoginForm() {
     }
   }
 
+  function handlePasswordConfirmation(text) {
+    setPasswordConfirmation(text)
+    if(password === text) {
+        console.log('passwords match')
+        setPasswordsMatch(true)
+    } else {
+        console.log('passwords dont match')
+        setPasswordsMatch(false)
+    }
+  }
+
   // Redirect to homepage after successful login
   useEffect(() => {
     if (registerSuccess) {
       alert('successful sign up. you may now login with these credentials')
       router.push('/Login')
     }
-  }, [registerSuccess])
+
+  }, [registerSuccess, passwordsMatch])
 
   // Clear fields and notify user of login failure
   const clearFields = () => {
@@ -119,6 +132,11 @@ export default function LoginForm() {
                   value={password}
                 /> 
               </div>
+              {!passwordsMatch && (
+                <div className="mb-2 flex items-center text-sm text-red-500">
+                  Passwords must match
+                </div>
+              )}
               <div className="mb-6 flex items-center text-lg md:mb-8">
                 <svg className="absolute ml-3" viewBox="0 0 24 24" width="24">
                   <path d="m18.75 9h-.75v-3c0-3.309-2.691-6-6-6s-6 2.691-6 6v3h-.75c-1.24 0-2.25 1.009-2.25 2.25v10.5c0 1.241 1.01 2.25 2.25 2.25h13.5c1.24 0 2.25-1.009 2.25-2.25v-10.5c0-1.241-1.01-2.25-2.25-2.25zm-10.75-3c0-2.206 1.794-4 4-4s4 1.794 4 4v3h-8zm5 10.722v2.278c0 .552-.447 1-1 1s-1-.448-1-1v-2.278c-.595-.347-1-.985-1-1.722 0-1.103.897-2 2-2s2 .897 2 2c0 .737-.405 1.375-1 1.722z" />
@@ -129,7 +147,7 @@ export default function LoginForm() {
                   name="passwordConfirmation"
                   className="w-full bg-gray-200 py-2 pl-12 focus:outline-offset-0 md:py-4"
                   placeholder="Enter your password again"
-                  onChange={(e) => setPasswordConfirmation(e.target.value)}
+                  onChange={(e) => handlePasswordConfirmation(e.target.value)}
                   value={passwordConfirmation}
                 />         
               </div>
