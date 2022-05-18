37d4d5b1227a66f047d4722e3afc99f591a074c1
TaylorWhite21 <taylorwhite2190@gmail.com>
2022-05-18 19:19:05 -0400

    Changed icon in navbar to cacheexplorer, fixed about us alignments.

--- components/Header.js
+++ components/Header.js
@@ -1,50 +1,47 @@
 import Link from 'next/link'
 import { useAuth } from '../contexts/auth'
 import axios from 'axios'
-import toast from "./ToastMessage";
+import toast from './ToastMessage'
 import React from 'react'
 
 // Environment variables
 const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
 
 export default function Header() {
-
   const { user, tokens, logout, refresh } = useAuth()
 
   // Toast notify message
   const notify = React.useCallback((type, message) => {
-    toast({ type, message });
-  }, []);
-
+    toast({ type, message })
+  }, [])
 
   // Dismiss toast
   const dismiss = React.useCallback(() => {
-    toast.dismiss();
-  }, []);
+    toast.dismiss()
+  }, [])
 
   const handleLogout = async () => {
-
     let options = {
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${tokens}`
-      }
+        Authorization: `Bearer ${tokens}`,
+      },
     }
 
     let endpoint = `${SERVER_URL}/api/user/logout/`
 
     // Server needs to know which refresh token to blacklist
     let data = {
-      'refresh': refresh
+      refresh: refresh,
     }
 
-    // Send server a request to blacklist the user's current refresh token 
+    // Send server a request to blacklist the user's current refresh token
     const response = await axios
       .post(endpoint, data, options)
       .then((res) => {
         // Perform actions after receiving response
         console.log(res)
-        notify("info", "Logged out.")
+        notify('info', 'Logged out.')
       })
       .catch((err) => {
         // POST failure
@@ -55,18 +52,19 @@ export default function Header() {
     logout()
   }
 
-
   return (
-    <nav className="px-2 py-5 text-white bg-gray-800 border-y-2 sm:px-4">
-      <div className="container flex flex-wrap items-center justify-between mx-auto">
+    <nav className="border-y-2 bg-gray-800 px-2 py-5 text-white sm:px-4">
+      <div className="container mx-auto flex flex-wrap items-center justify-between">
         <Link href="/">
           <a href="https://placeholder.com">
-            <img src="https://via.placeholder.com/50"></img>
+            <h3 className="p-5 text-center font-semibold text-black">
+              Cach<span className="text-green-600">E</span>xplorer
+            </h3>
           </a>
         </Link>
-        <span className="self-center text-xl font-semibold whitespace-nowrap"></span>
+        <span className="self-center whitespace-nowrap text-xl font-semibold"></span>
         <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
-          <ul className="flex flex-col mt-4 md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium">
+          <ul className="mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium">
             <li>
               <Link href="/">
                 <a className="hover:text-green-600">Home</a>
@@ -84,28 +82,29 @@ export default function Header() {
             </li>
 
             {/* Don't show login field if user is logged in */}
-            {!user?.username &&
+            {!user?.username && (
               <li>
                 <Link href="/Login">
                   <a className="hover:text-green-600">Login</a>
                 </Link>
               </li>
-            }
-            {!user?.username &&
+            )}
+            {!user?.username && (
               <li>
                 <Link href="/SignUp">
                   <a className="hover:text-green-600">Sign up</a>
                 </Link>
               </li>
-            }
-            {user?.username &&
+            )}
+            {user?.username && (
               <li>
                 <Link href="/">
-                  <a className="hover:text-green-600"
-                    onClick={handleLogout}>Logout</a>
+                  <a className="hover:text-green-600" onClick={handleLogout}>
+                    Logout
+                  </a>
                 </Link>
               </li>
-            }
+            )}
           </ul>
         </div>
       </div>
